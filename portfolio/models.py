c23f3b21e3f6a773f911b4df3c7056ccd44b3c23
IDOMATH <bmtschida@stcloudstate.edu>
2020-05-04 22:05:18 -0500

    added portfolio page

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-#inherits from above model class
+
 class Project(models.Model):
+
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
