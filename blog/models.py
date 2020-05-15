3e01d9c58b75218bac9a7d3e93879cb22c932b0a
IDOMATH <bmtschida@stcloudstate.edu>
2020-05-15 00:58:43 -0500

    Wrapping up customizations in bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ("draft", "Draft"),
+        ("published", "Published"),
+    )
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    description = models.CharField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
+    slug = models.SlugField(max_length=50, unique_for_date="publish")
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
--- portfolio/models.py
+++ portfolio/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class Project(models.Model):
-
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
